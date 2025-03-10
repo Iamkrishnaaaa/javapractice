73e1cf91ea10d16b8c82f249c37cc7774fb1b9e8
Iamkrishnaaaa <iamkrish423@gmail.com>
2025-03-10 07:41:13 +0530

    pattern.java

--- pattern.java
+++ pattern.java
@@ -1,1 +1,11 @@
-"This is a new file" 
+class pattern{
+    public static void main(String[] args) {
+       System.out.println("pattern program");
+       for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+       } 
+    }
+}
